fdc6a144017744b9c80349c6e8b61e8fda585f68
wanpracha <wanpracha.n@rmutsv.ac.th>
2019-11-14 13:11:58 +0700

    Control Success

--- lib/widget/control.dart
+++ lib/widget/control.dart
@@ -15,6 +15,8 @@ class _ControlState extends State<Control> {
       lightBool = false,
       waterBool = false;
 
+  int modeInt = 0, fogInt = 0, fanInt = 0, waterInt = 0, lightInt = 0;
+
   IotModel iotModel;
 
   //Method
@@ -31,38 +33,42 @@ class _ControlState extends State<Control> {
     await databaseReference.once().then((DataSnapshot dataSnapshot) {
       print('dataSnapshot = ${dataSnapshot.value}');
       iotModel = IotModel.formMap(dataSnapshot.value);
+      modeInt = iotModel.mode;
+      fogInt = iotModel.fog;
+      fanInt = iotModel.fan;
+      waterInt = iotModel.water;
+      lightInt = iotModel.light;
       checkSwitch();
     });
   }
 
   void checkSwitch() {
     setState(() {
-      
-    if (iotModel.mode == 0) {
-      modeBool = false;
-    } else {
-      modeBool = true;
-    }
-    if (iotModel.fog == 0) {
-      fogBool = false;
-    } else {
-      fogBool = true;
-    }
-    if (iotModel.fan == 0) {
-      fanBool = false;
-    } else {
-      fanBool = true;
-    }
-    if (iotModel.water == 0) {
-      waterBool = false;
-    } else {
-      waterBool = true;
-    }
-    if (iotModel.light == 0) {
-      lightBool = false;
-    } else {
-      lightBool = true;
-    }
+      if (iotModel.mode == 0) {
+        modeBool = false;
+      } else {
+        modeBool = true;
+      }
+      if (iotModel.fog == 0) {
+        fogBool = false;
+      } else {
+        fogBool = true;
+      }
+      if (iotModel.fan == 0) {
+        fanBool = false;
+      } else {
+        fanBool = true;
+      }
+      if (iotModel.water == 0) {
+        waterBool = false;
+      } else {
+        waterBool = true;
+      }
+      if (iotModel.light == 0) {
+        lightBool = false;
+      } else {
+        lightBool = true;
+      }
     });
     print(
         'Mode = $modeBool,Fog = $fogBool, Fan = $fanBool,Water = $waterBool, Light = $lightBool');
@@ -82,7 +88,13 @@ class _ControlState extends State<Control> {
                 Switch(
                   value: modeBool,
                   onChanged: (bool value) {
-                    changeBool(value);
+                    if (value) {
+                      modeInt = 1;
+                      changeModeBool(value);
+                    } else {
+                      modeInt = 0;
+                      changeModeBool(value);
+                    }
                   },
                 ),
                 Text('Manual'),
@@ -108,7 +120,13 @@ class _ControlState extends State<Control> {
                 Switch(
                   value: fogBool,
                   onChanged: (bool value) {
-                    changeBool(value);
+                    if (value) {
+                      fogInt = 1;
+                      changeFogBool(value);
+                    } else {
+                      fogInt = 0;
+                      changeFogBool(value);
+                    }
                   },
                 ),
                 Text('ON'),
@@ -134,7 +152,13 @@ class _ControlState extends State<Control> {
                 Switch(
                   value: fanBool,
                   onChanged: (bool value) {
-                    changeBool(value);
+                    if (value) {
+                      fanInt = 1;
+                      changeFanBool(value);
+                    } else {
+                      fanInt = 0;
+                      changeFanBool(value);
+                    }
                   },
                 ),
                 Text('ON'),
@@ -160,7 +184,13 @@ class _ControlState extends State<Control> {
                 Switch(
                   value: waterBool,
                   onChanged: (bool value) {
-                    changeBool(value);
+                    if (value) {
+                      waterInt = 1;
+                      changeWaterBool(value);
+                    } else {
+                      waterInt = 0;
+                      changeWaterBool(value);
+                    }
                   },
                 ),
                 Text('ON'),
@@ -190,7 +220,13 @@ class _ControlState extends State<Control> {
                 Switch(
                   value: lightBool,
                   onChanged: (bool value) {
-                    changeBool(value);
+                    if (value) {
+                      lightInt = 1;
+                      changeLightBool(value);
+                    } else {
+                      lightInt = 0;
+                      changeLightBool(value);
+                    }
                   },
                 ),
                 Text('ON'),
@@ -202,14 +238,61 @@ class _ControlState extends State<Control> {
     );
   }
 
-  void changeBool(bool value) {
+  Future<void> editDatabase() async {
+    FirebaseDatabase firebaseDatabase = FirebaseDatabase.instance;
+    DatabaseReference databaseReference =
+        firebaseDatabase.reference().child('IoT');
+    Map<dynamic, dynamic> map = Map();
+    map['Water'] = waterInt;
+    map['Fan'] = fanInt;
+    map['Temp_High'] = iotModel.tempHigh;
+    map['Humidity_High'] = iotModel.humidityHigh;
+    map['Humidity_Low'] = iotModel.humidityLow;
+    map['Light'] = lightInt;
+    map['Mode'] = modeInt;
+    map['Temp_Low'] = iotModel.tempLow;
+    map['Suitable_Humi'] = iotModel.suitableHumi;
+    map['Fog'] = fogInt;
+    map['Suitable_Tem'] = iotModel.suitableTem;
+
+    await databaseReference.set(map).then((response){
+      print('Edit Success');
+    });
+  }
+
+  void changeModeBool(bool value) {
     setState(() {
+      //redrawing screen
       modeBool = value;
-      // fanBool = value;
-      // fogBool = value;
-      // waterBool = value;
-      // lightBool = value;
-      print('modeBool = $modeBool');
+      editDatabase();
+    });
+  }
+
+  void changeFogBool(bool value) {
+    setState(() {
+      fogBool = value;
+      editDatabase();
+    });
+  }
+
+  void changeFanBool(bool value) {
+    setState(() {
+      fanBool = value;
+      editDatabase();
+    });
+  }
+
+  void changeLightBool(bool value) {
+    setState(() {
+      lightBool = value;
+      editDatabase();
+    });
+  }
+
+  void changeWaterBool(bool value) {
+    setState(() {
+      waterBool = value;
+      editDatabase();
     });
   }
 
